afe6121a87c54c1e948e238bd71cba68c52f6ad7
Brad Morgan <brad@dmgctrl.com>
2011-09-20 14:53:49 -0400

    Cleaned up tab strip view

--- src/chrome/browser/cocoa/CTTabStripView.h
+++ src/chrome/browser/cocoa/CTTabStripView.h
@@ -1,18 +1,5 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE-chromium file.
-
-#ifndef CHROME_BROWSER_COCOA_TAB_STRIP_VIEW_H_
-#define CHROME_BROWSER_COCOA_TAB_STRIP_VIEW_H_
-#pragma once
-
-#import <Cocoa/Cocoa.h>
-
 
 @class NewTabButton;
-
-// A view class that handles rendering the tab strip and drops of URLS with
-// a positioning locator for drop feedback.
 
 @interface CTTabStripView : NSView
 
@@ -22,11 +9,9 @@
 
 @end
 
-// Protected methods subclasses can override to alter behavior. Clients should
-// not call these directly.
-@interface CTTabStripView(Protected)
+@interface CTTabStripView (Protected)
+
 - (void)drawBottomBorder:(NSRect)bounds;
 - (BOOL)doubleClickMinimizesWindow;
-@end
 
-#endif  // CHROME_BROWSER_COCOA_TAB_STRIP_VIEW_H_
+@end

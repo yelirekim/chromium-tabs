1c5ad626c19963ea884dd619eec176e641905f0f
Brad Morgan <brad@dmgctrl.com>
2011-09-21 10:20:29 -0400

    Cleaned up hover button code

--- src/chrome/browser/cocoa/HoverButton+Private.h
+++ src/chrome/browser/cocoa/HoverButton+Private.h
@@ -1,7 +1,5 @@
 #import "HoverButton.h"
 
-// Enumeration of the hover states that the close button can be in at any one
-// time. The button cannot be in more than one hover state at a time.
 typedef enum {
     kHoverStateNone = 0,
     kHoverStateMouseOver = 1,
--- src/chrome/browser/cocoa/HoverButton.h
+++ src/chrome/browser/cocoa/HoverButton.h
@@ -1,19 +1,7 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE-chromium file.
 
-#import <Cocoa/Cocoa.h>
-
-
-// A button that changes when you hover over it and click it.
 @interface HoverButton : NSButton
 
-// Enables or disables the |NSTrackingRect|s for the button.
 - (void)setTrackingEnabled:(BOOL)enabled;
-
-// Checks to see whether the mouse is in the button's bounds and update
-// the image in case it gets out of sync.  This occurs to the close button
-// when you close a tab so the tab to the left of it takes its place, and
-// drag the button without moving the mouse before you press the button down.
 - (void)checkImageState;
+
 @end
